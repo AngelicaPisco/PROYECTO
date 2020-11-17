3c3423ce8e0771a38bfe4bb4a7f57dfc79b05d7e
Angelicapisco <angelica.pisc8934@utc.edu.ec>
2020-11-17 17:27:52 -0500

    modificado comando

--- hola.cpp
+++ hola.cpp
@@ -3,3 +3,27 @@
 main(){
 	printf("Hola Mundo");
 }
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	printf("Hola Mundo");
+}
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	printf("Hola Mundo");
+}
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	printf("Hola Mundo");
+}
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	printf("Hola Mundo");
+}
